b1ef378651aacbf5c39a9259485eee5c77190de1
Mahesh Sadupalli <132281261+mahesh-sadupalli@users.noreply.github.com>
2024-08-30 21:52:57 +0200

    Add files via upload

---
+++ Geeks.java
@@ -0,0 +1,15 @@
+class Geeks{
+    
+    static void printIndividualLine(){
+        
+        //Printing Geeks in a new Line
+        System.out.println("Geeks");
+        
+        //Printing for in a new Line
+        System.out.println("for");
+        
+        //Printing Geeks in a new Line
+        System.out.println("Geeks");
+    }
+    
+}
